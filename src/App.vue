86cfbd433fb424b86732bb4f4d7d0822a7c69949
ilrdn <49016594+ilrdn@users.noreply.github.com>
2020-12-08 16:19:58 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Contacts</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@import url(./assets/scss/main.scss);
+</style>
