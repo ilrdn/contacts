f02d8188364e25956aa931dbd3cce41cce97187a
ilrdn <49016594+ilrdn@users.noreply.github.com>
2020-12-10 02:25:06 +0400

    some ui

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,69 @@
 <template>
-  <div id="app">
+  <div class="container">
     <h1>Contacts</h1>
+    <div class="flex-container contacts">
+        <a class="flex-item contact" v-for="contact in contacts" :key="contact.id">
+          <div class="contact__img">
+            <span>
+              {{ contact.name | capitalize }}
+            </span>
+            <div class="fav" v-if="contact.fav"></div>
+          </div>
+          <div class="contact__info">
+            <h2>{{ contact.name }}</h2>
+            <span v-if="contact.phone">{{ contact.phone }}</span>
+          </div>
+          <button class="contact__del"></button>
+        </a>
+        <button @click="showModal = true" class="flex-item addBtn"><span>Add contact</span></button>
+    </div>
+    <modal v-if="showModal">
+      <h3 slot="header">custom header</h3>
+      <button class="modal-default-button" @click="showModal = false">
+        OK
+      </button>
+    </modal>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data () {
+    return {
+      showModal: false,
+      contacts: [
+        {
+          'id': 1,
+          'name': 'admin',
+          'company': 'contacts',
+          'phone': '+7 (999) 999 99 99',
+          'fav': false
+        },
+        {
+          'id': 2,
+          'name': 'Ilya Rodin',
+          'phone': '+7 (999) 999 99 99',
+          'fav': true
+        },
+        {
+          'id': 3,
+          'name': 'name',
+          'phone': '+7 (999) 999 99 99',
+          'fav': false
+        }
+      ]
+    }
+  },
+  filters: {
+    capitalize: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.slice(0, 1).toUpperCase()
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-@import url(./assets/scss/main.scss);
+@import "./assets/scss/main.scss";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+/* import Contact from '@/components/Contact' */
 
 Vue.use(Router)
 
